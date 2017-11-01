d2a81bec466dd29fac2e75e0239d23444cc87d94
jack <h_7357@qq.com>
2017-11-01 10:29:29 +0800

    add httpclient to client struct

--- yingyan.go
+++ yingyan.go
@@ -8,35 +8,42 @@ import (
 )
 
 type Client struct {
-	ak        string
-	sk        string
-	serviceID int
-	s         bool
+	ak         string
+	sk         string
+	httpClient *http.Client
+	serviceID  int
+	s          bool
 }
 
 // visit http://lbsyun.baidu.com/apiconsole/key/ 获取ak
 // 如果设置的白名单则设置sk ""
 func NewClient(ak, sk string, serviceID int) *Client {
 	return &Client{
-		sk:        sk,
-		ak:        ak,
-		s:         true,
-		serviceID: serviceID,
+		sk:         sk,
+		ak:         ak,
+		s:          true,
+		serviceID:  serviceID,
+		httpClient: &http.Client{},
 	}
 }
 
-func (serv *Client) Post(path string, param map[string]string) (body []byte, err error) {
+// SetHttpClient you can set your own http client
+func (c *Client) SetHttpClient(httpClient *http.Client) {
+	c.httpClient = httpClient
+}
+
+func (c *Client) Post(path string, param map[string]string) (body []byte, err error) {
 	data := url.Values{}
-	data.Add("ak", serv.ak)
-	data.Add("service_id", strconv.Itoa(serv.serviceID))
+	data.Add("ak", c.ak)
+	data.Add("service_id", strconv.Itoa(c.serviceID))
 	for k, v := range param {
 		data.Add(k, v)
 	}
-	sn := serv.sign(path, &data)
+	sn := c.sign(path, &data)
 	if sn != "" {
 		data.Add("sn", sn)
 	}
-	resp, err := http.PostForm(apiRootPath+path, data)
+	resp, err := c.httpClient.PostForm(apiRootPath+path, data)
 	if err != nil {
 		return nil, err
 	}
@@ -44,18 +51,18 @@ func (serv *Client) Post(path string, param map[string]string) (body []byte, err
 	return ioutil.ReadAll(resp.Body)
 }
 
-func (serv *Client) Get(path string, param map[string]string) (body []byte, err error) {
+func (c *Client) Get(path string, param map[string]string) (body []byte, err error) {
 	data := url.Values{}
-	data.Add("ak", serv.ak)
-	data.Add("service_id", strconv.Itoa(serv.serviceID))
+	data.Add("ak", c.ak)
+	data.Add("service_id", strconv.Itoa(c.serviceID))
 	for k, v := range param {
 		data.Add(k, v)
 	}
-	sn := serv.sign(path, &data)
+	sn := c.sign(path, &data)
 	if sn != "" {
 		data.Add("sn", sn)
 	}
-	resp, err := http.Get(apiRootPath + path + "?" + data.Encode())
+	resp, err := c.httpClient.Get(apiRootPath + path + "?" + data.Encode())
 	if err != nil {
 		return nil, err
 	}
